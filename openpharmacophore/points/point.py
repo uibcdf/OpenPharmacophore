4d08bae9e130e3c3a817649f0e4d14cd3e4aeb40
Diego Prada <prada.gracia@gmail.com>
2021-07-05 20:38:12 -0500

    first point added :)

---
+++ openpharmacophore/points/point.py
@@ -0,0 +1,11 @@
+class Point():
+
+""" Parent class of pharmacophoric point.
+Common attributes and methods will be included here to be inherit by the specific pharmacophoric
+points classes.
+"""
+
+    def __init__(self):
+
+        self.position = None
+
