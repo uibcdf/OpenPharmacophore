86319ede2e4500f84a619ec6d3756320fb126b19
Daniel-Ibarrola <dis96051@gmail.com>
2022-08-15 22:54:23 -0500

    Data files are loaded from a module in the data folder. Fixes bug when lodading files in tests

---
+++ openpharmacophore/tests/test_load_data_files.py
@@ -0,0 +1,33 @@
+import openpharmacophore.data as data
+
+
+def test_all_data_dictionaries_are_populated():
+
+    assert len(data.bioassays) == 2
+    assert len(data.ligands) == 5
+    assert len(data.pdb) == 5
+    assert len(data.pharmacophores) == 5
+    assert len(data.trajectories) == 2
+
+
+def test_can_load_a_file_from_each_dictionary():
+
+    molecules = data.ligands["mols"]
+    with open(molecules) as fp:
+        lines = fp.readlines()
+        assert len(lines) == 5
+
+    pdb_1ncr = data.pdb["1ncr"]
+    with open(pdb_1ncr) as fp:
+        header = fp.readline()
+        assert "HEADER" in header
+
+    pharmacophore = data.pharmacophores["elastase"]
+    with open(pharmacophore) as fp:
+        header = fp.readline()
+        assert "@<TRIPOS>" in header
+
+    topology = data.trajectories["er_alpha"]
+    with open(topology) as fp:
+        header = fp.readline()
+        assert "54437" in header
