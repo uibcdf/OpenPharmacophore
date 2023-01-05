e836a36d029d535bb4d70330c99392647b715c78
Daniel-Ibarrola <dis96051@gmail.com>
2023-01-05 14:23:18 -0600

    Deleted _pyunitwizard folder. PyunitWizard config is now in the main module init

--- openpharmacophore/_pyunitwizard/__init__.py
+++
@@ -1,6 +0,0 @@
-import pyunitwizard as puw
-puw.configure.load_library(['pint', "openmm.unit"])
-puw.configure.set_default_form('pint')
-puw.configure.set_standard_units(['angstroms', 'ps', 'K', 'mole', 'amu', 'e',
-                                 'kJ/mol', 'kJ/(mol*nm**2)', 'N', 'degrees'])
-
