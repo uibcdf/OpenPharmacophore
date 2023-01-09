933f5d16b5d49da10f592db65425ad35b88ef89f
Daniel-Ibarrola <dis96051@gmail.com>
2023-01-09 10:18:25 -0600

    np.delete function for a quantity

--- openpharmacophore/utils/maths.py
+++ openpharmacophore/utils/maths.py
@@ -126,8 +126,24 @@ def maximum_distance(centroid, coords):
     return np.amax(distance)
 
 
-def delete(quantity, indices):
+def delete(quantity, indices, axis=None):
     """ Return a new array with sub-arrays along an axis deleted.
         For a one dimensional array, this returns those entries not
-        returned by arr[indices]."""
-    pass
+        returned by arr[indices].
+
+        Parameters
+        ----------
+        quantity : puw.Quantity
+
+        indices : list[int]
+
+        axis : int, optional
+
+        Returns
+        -------
+        puw.Quantity
+            The new quantity.
+    """
+    unit = str(puw.get_unit(quantity))
+    sub_array = np.delete(puw.get_value(quantity), indices, axis=axis)
+    return puw.quantity(sub_array, unit)
---
+++ tests/unit/molecular_systems/test_ligand.py
@@ -0,0 +1,5 @@
+from openpharmacophore.molecular_systems.ligand import Ligand, ligand_from_topology
+
+
+def test_ligand_from_topology():
+    pass
