1296628a44261e71cfabd549f9a2ff6bb18a0f79
Daniel-Ibarrola <dis96051@gmail.com>
2023-01-18 14:11:27 -0600

    Modified PL complex preparation test to also extract binding site and moved it to binding site test file

--- tests/integration/test_lr_dynamic_pharmacophore.py
+++ tests/integration/test_lr_dynamic_pharmacophore.py
@@ -7,20 +7,19 @@ def test_dynamic_ligand_receptor_pharmacophore(traj_er_alpha):
     # that consists of three frames
     protein = oph.load(traj_er_alpha)
     # We know that the file contains a single ligand
-    assert protein.has_ligands
+    lig_ids = protein.ligand_ids
+    assert len(lig_ids) == 1
     # The receptor already contains hydrogens
     assert protein.has_hydrogens
 
-    lig_ids = protein.ligand_ids
-    assert len(lig_ids) == 1
-
     ligand = protein.get_ligand(lig_ids[0], remove_hyd=False)
+    # Ligand already contains hydrogens
+    assert ligand.has_hydrogens
+    assert ligand.n_atoms == 44
     ligand.fix_bond_order(
         smiles="C[C@]12CC[C@@H]3c4ccc(cc4CC[C@H]3[C@@H]1CC[C@@H]2O)O"
     )
-    # Ligand already contains hydrogens
-    assert ligand.has_hydrogens()
-    assert ligand.n_atoms == 44
+    assert ligand.has_aromatic_bonds()
 
     # We extract the pharmacophore
     pharmacophore = oph.LigandReceptorPharmacophore(protein, ligand)
